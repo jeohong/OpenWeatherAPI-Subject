0b018dbcb0606ea337a5f453407de35780f4c187
jeohong <jeohong59@gmail.com>
2023-02-14 14:58:02 +0900

    FEAT : MainView SearchBar 구현

--- AutocryptSubject/Views/MainView/MainViewController.swift
+++ AutocryptSubject/Views/MainView/MainViewController.swift
@@ -8,8 +8,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    // Component
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
 
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            textfield.backgroundColor = .theme.searchBarBackground
+
+            let tapped = UITapGestureRecognizer(target: self, action: #selector(presentSearchView))
+            textfield.addGestureRecognizer(tapped)
+        }
+
+        return searchBar
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .theme.mainViewBackground
+        
+        self.navigationItem.titleView = searchBar
+    }
+
+    @objc func presentSearchView() {
+        print("Navigated SearchViewController")
     }
 }
