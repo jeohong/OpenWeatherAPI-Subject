7056421e8358a4b9597e0087963528c0125ec699
jeohong <jeohong59@gmail.com>
2023-02-15 15:30:50 +0900

    FEAT : CollectionView의 편의 사용을 위한 Extension 추가

---
+++ AutocryptSubject/Extentions/UICollectionReuableView+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionReuableView+.swift
+//  AutocryptSubject
+//
+//  Created by Hong jeongmin on 2023/02/15.
+//
+
+import UIKit
+
+extension UICollectionReusableView {
+    static var identifier: String {
+        return String(describing: Self.self)
+    }
+}
