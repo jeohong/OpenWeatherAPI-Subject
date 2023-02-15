1a5bb755b17b7e14cee9eb3c6d5bc2d093b1b7b1
jeohong <jeohong59@gmail.com>
2023-02-15 15:57:40 +0900

    FEAT : WeatherCollectionViewCell 파일 생성 및 구현

---
+++ AutocryptSubject/Views/MainView/WeatherCollectionViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  WeatherCollectionViewCell.swift
+//  AutocryptSubject
+//
+//  Created by Hong jeongmin on 2023/02/15.
+//
+
+import UIKit
+import SnapKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .theme.footnote
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    let weatherIcon: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = .theme.footnote
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        self.addSubview(timeLabel)
+        timeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview()
+        }
+        
+        self.addSubview(weatherIcon)
+        timeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(timeLabel.snp.bottom).offset(5)
+        }
+        
+        self.addSubview(temperatureLabel)
+        timeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(weatherIcon.snp.bottom).offset(5)
+        }
+    }
+}
